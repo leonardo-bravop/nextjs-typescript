f1dac4266a9bf9069f788d5792ace01fd3771179
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-07-05 13:11:06 -0300

    Created single task endpoint

--- pages/api/tasks/[id].ts
+++ pages/api/tasks/[id].ts
@@ -1,19 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { conn } from "../../../utils/database"
 
-const idFunc = (req: NextApiRequest, res: NextApiResponse) => {
+const idFunc = async (req: NextApiRequest, res: NextApiResponse) => {
 
-    const { method } = req
+    const { method, query, body } = req
     const { id } = req.query
 
     switch (method) {
         case 'GET':
-            return res.status(200).json(`gettin task ${id}`)
+            try {
+                const text = 'SELECT * FROM tasks WHERE id = $1'
+                const values = [query.id]
+                const result = await conn.query(text, values)
+                if (result.rows.length === 0) {
+                    return res.status(404).json({ message: "Task not found" })
+                }
+                return res.json(result.rows[0])
+            }
+            catch (error: any) {
+                return res.status(500).json({ message: error.message })
+            }
 
         case 'PUT':
-            return res.status(200).json(`edit task ${id}`)
+            try {
+                const { title, description } = body
+                const text = 'UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *'
+                const values = [title, description, query.id]
+                const result = await conn.query(text, values)
+                if (result.rows.length === 0) {
+                    return res.status(404).json({ message: "Task not found" })
+                }
+                return res.json(result.rows[0])
+            }
+            catch (error: any) {
+                return res.status(500).json({ message: error.message })
+            }
 
         case 'DELETE':
-            return res.status(200).json(`delete  task ${id}`)
+            try {
+                const text = 'DELETE FROM tasks WHERE id = $1 RETURNING *'
+                const values = [query.id]
+                const result = await conn.query(text, values)
+                if (result.rowCount === 0) {
+                    return res.status(404).json({ message: "Task not found" })
+                }
+                return res.json(result.rows[0])
+            }
+            catch (error: any) {
+                return res.status(500).json({ message: error.message })
+            }
 
         default:
             return res.status(400).json('invalid method');
